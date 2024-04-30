3df3fdb965665c358f2aea6204fc0299581d5802
zuki <zukidono@gmail.com>
2024-04-30 15:50:16 +0700

    added Test Jobarranger

--- ja_test.go
+++ ja_test.go
@@ -11,7 +11,7 @@ type ja_test interface {
 	TestJaz() bool
 }
 
-func TestCallTest(t *testing.T) {
+func TestJobarranger(t *testing.T) {
 	var tests []ja_test
 	tests = append(tests, new(jalibs.Jalockutil_ja_test)) //add more test cases' object here
 
