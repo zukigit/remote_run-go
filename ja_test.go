e5e8cbf6a4c4c59ee8697d305ee9964c22bb714f
linnalastor <linnalator@gmail.com>
2024-05-03 14:18:10 +0630

    added abort

--- ja_test.go
+++ ja_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	jalibs "pro2.jobarranger.info/jobarranger/test/src_t/jalibs_t"
+	jobarg_agentd "pro2.jobarranger.info/jobarranger/test/src_t/jobarg_agentd"
 )
 
 type ja_test interface {
@@ -14,6 +15,7 @@ type ja_test interface {
 func TestJobarranger(t *testing.T) {
 	var tests []ja_test
 	tests = append(tests, new(jalibs.Jalockutil_ja_test)) //add more test cases' object here
+	tests = append(tests, new(jobarg_agentd.Jajobfile))
 
 	for _, e := range tests {
 		if !e.TestJaz() {
