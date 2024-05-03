be521abe057df70bfd27e727dc8fb9fdaff039f1
linnalastor <linnalator@gmail.com>
2024-05-03 16:55:00 +0630

    added some shit

--- common.go
+++ common.go
@@ -2,12 +2,13 @@ package test
 
 /*
 #cgo CFLAGS: -g -Wall
-#cgo LDFLAGS: -L../ -l:./src/jalibs/libjacommon_a-jalockutil.o -l:./src/libs/zbxcommon/str.o -l:./src/jalibs/libjacommon_a-jafile.o -l:./src/libs/zbxlog/log.o -l:./tools/json-c-0.9/json_object.o -l:./tools/json-c-0.9/printbuf.o -l:./tools/json-c-0.9/arraylist.o -l:./tools/json-c-0.9/linkhash.o -l:./src/libs/zbxsys/threads.o -l:./src/libs/zbxcommon/misc.o -l:./src/libs/zbxsys/mutexs.o
+#cgo LDFLAGS: -L../ -l:./src/jalibs/libjacommon_a-jalockutil.o -l:./src/libs/zbxcommon/str.o -l:./src/jalibs/libjacommon_a-jafile.o -l:./src/libs/zbxlog/log.o -l:./tools/json-c-0.9/json_object.o -l:./tools/json-c-0.9/printbuf.o -l:./tools/json-c-0.9/arraylist.o -l:./tools/json-c-0.9/linkhash.o -l:./src/libs/zbxsys/threads.o -l:./src/libs/zbxcommon/misc.o -l:./src/libs/zbxsys/mutexs.o -l:./src/jalibs/libjacommon_a-jajobfile.o
 
 char	*CONFIG_LOG_FILE	 = "/var/log/jobarranger/jobarg_server.log";
 int CONFIG_DB_CON_COUNT = 10;
 char	*CONFIG_TMPDIR = "/var/lib/jobarranger/tmp/";
 int	CONFIG_LOG_FILE_SIZE	= 1;
+int JA_FILE_PATH_LEN = 260;
 char	*CONFIG_FILE		 = "/home/zuki/Documents/dev/work/jobarranger-6.0.5.1/jaconf/jobarg_server.conf";
 
 const char	title_message[] = "Job Arranger Server";
--- src_t/jobarg_agentd/jajobfile_t.go
+++ src_t/jobarg_agentd/jajobfile_t.go
@@ -2,6 +2,42 @@ package jalibs_t
 
 /*
 typedef unsigned long int zbx_uint64_t;
+
+typedef struct {
+    char		kind[16];
+    double		version;
+    zbx_uint64_t	jobid;
+    char		serverid[33];
+    char		hostname[128];
+    int			method;
+    char		type[1024];
+    char		argument[4096];
+    char		script[4096];
+    char		env[132097];
+    int			result;
+    int			status;
+    int		pid;
+    zbx_uint64_t	start_time;
+    zbx_uint64_t	end_time;
+    char		message[1024];
+    char		std_out[64001];
+    char		std_err[64001];
+    int			return_code;
+    int			signal;
+    int			send_retry;
+    char		run_user[1024];
+    char		run_user_password[1024];
+    int			loop_cnt;
+
+    char cur_unique_id[1024];
+    char pre_unique_id[1024];
+
+    char       serverip[33];
+    // zbx_uint64_t    host_running_job[512];
+    zbx_uint64_t*    host_running_job;
+    int         size_of_host_running_job;
+} ja_job_object;
+int JA_FILE_PATH_LEN = 260;
 #include "../../../jainclude/jajobfile.h"
 */
 import "C"
@@ -12,11 +48,11 @@ type Jajobfile struct{}
 func check_job_abort_flag_test() bool {
 	const __function_name = "check_job_abort_flag_test"
 	var rtn bool = false
-	// var failedTestCase string
+	var failedTestCase string
 
-	// if C.check_job_abort_flag(0) == 0 {
-	// 	fmt.Printf("Test case 4.1 was successfully executed. \n", __function_name)
-	// } else {
+	if C.check_job_abort_flag(failedTestCase, 0) == 0 {
+		fmt.Printf("Test case 4.1 was successfully executed. \n", __function_name)
+	} //else {
 	// 	fmt.Printf("Test case 4.1 execution was failed. \n", __function_name)
 	// 	failedTestCase += "4.1"
 	// 	return false
