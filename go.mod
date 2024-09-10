69f4079b1676b8abc896b03ae774334ed2fad8d7
zuki <zukidono@gmail.com>
2024-09-10 23:43:54 +0700

    asking host and password is finished

--- go.mod
+++ go.mod
@@ -4,4 +4,7 @@ go 1.23.1
 
 require golang.org/x/term v0.24.0
 
-require golang.org/x/sys v0.25.0 // indirect
+require (
+	golang.org/x/crypto v0.27.0 // indirect
+	golang.org/x/sys v0.25.0 // indirect
+)
--- remote_run.go
+++ remote_run.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 	"syscall"
 
@@ -10,22 +11,41 @@ import (
 )
 
 func main() {
-	if len(os.Args) != 2 {
-		fmt.Println("Usage: remote_run user@host")
+	arg_len := len(os.Args)
+	var host string
+	port := "22"
+
+	// check args
+	if arg_len == 2 {
+		host = os.Args[1]
+	} else if arg_len == 3 {
+		host = os.Args[1]
+		port = os.Args[2]
+
+		// check port
+		if _, err := strconv.Atoi(port); err != nil {
+			fmt.Printf("Error: Bad port '%s'\n", port)
+			os.Exit(1)
+		}
+	} else {
+		fmt.Println("Usage: remote_run user@host <port>")
 		os.Exit(1)
 	}
 
-	parts := strings.Split(os.Args[1], "@")
+	// split user and host
+	parts := strings.Split(host, "@")
 	if len(parts) != 2 {
-		fmt.Println("Usage: remote_run user@host")
+		fmt.Println("Usage: remote_run user@host <port>")
 		os.Exit(1)
 	}
-	user := parts[0]
-	host := parts[1]
 
+	user := parts[0]
+	host = parts[1] + ":" + port
+
+	// get password
 	fmt.Printf("%s's password:", os.Args[1])
 	bytepw, err := term.ReadPassword(int(syscall.Stdin))
-	fmt.Println()
+	fmt.Println() //new line
 	if err != nil {
 		fmt.Println("Error:", err.Error())
 		os.Exit(1)
