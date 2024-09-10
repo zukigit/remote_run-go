b0c69d66a3426a52cd83270e0ccecfb15d926d46
zuki <zukidono@gmail.com>
2024-09-10 13:35:03 +0700

    added user name password util

--- go.mod
+++ go.mod
@@ -1,8 +1,7 @@
-module zukigit/remote_test-go
+module zukigit/remote_run-go
 
 go 1.23.1
 
-require (
-	golang.org/x/crypto v0.27.0 // indirect
-	golang.org/x/sys v0.25.0 // indirect
-)
+require golang.org/x/term v0.24.0
+
+require golang.org/x/sys v0.25.0 // indirect
--- remote_run.go
+++ remote_run.go
@@ -2,8 +2,37 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strings"
+	"syscall"
+
+	"golang.org/x/term"
 )
 
 func main() {
-	fmt.Println("hello world")
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: remote_run user@host")
+		os.Exit(1)
+	}
+
+	parts := strings.Split(os.Args[1], "@")
+	if len(parts) != 2 {
+		fmt.Println("Usage: remote_run user@host")
+		os.Exit(1)
+	}
+	user := parts[0]
+	host := parts[1]
+
+	fmt.Printf("%s's password:", os.Args[1])
+	bytepw, err := term.ReadPassword(int(syscall.Stdin))
+	fmt.Println()
+	if err != nil {
+		fmt.Println("Error:", err.Error())
+		os.Exit(1)
+	}
+	pass := string(bytepw)
+
+	fmt.Println("user", user)
+	fmt.Println("host", host)
+	fmt.Println("pass", pass)
 }
