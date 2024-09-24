0b59c2defb84cb3774e5b5e58a01e0ad964bb3bc
zuki <zukidono@gmail.com>
2024-09-24 22:42:13 +0700

    ticket 168 169 170

--- src/tickets/commom.go
+++ src/tickets/commom.go
@@ -6,4 +6,7 @@ const (
 	PASSED     dao.Testcase_status = "PASSED"
 	FAILED     dao.Testcase_status = "FAILED"
 	MUST_CHECK dao.Testcase_status = "MUST_CHECK"
+
+	END    string = "END"
+	NORMAL string = "NORMAL"
 )
--- src/tickets/ticket_1318.go
+++ src/tickets/ticket_1318.go
@@ -56,49 +56,113 @@ func (t *Ticket_1318) Run() {
 func (t *Ticket_1318) Add_testcases() {
 	// Add your test case here
 	// ticket 168
-	// tc_170 := t.New_testcase(170, "Default Case Check. Linux AGENT")
-	// tc_func := func() dao.Testcase_status {
-	// 	std_out, error := tc_170.Jobarg_exec("TICKET_1318_linux")
-	// 	if error != nil {
-	// 		tc_170.Err_log("Error: %s, std_out: %s", error.Error(), std_out)
-	// 		return FAILED
-	// 	}
+	tc_168 := t.New_testcase(169, "Abnormal Case") // create test case
+	tc_func := func() dao.Testcase_status {
 
-	// 	jobnet_info, error := tc_170.Jobarg_get_jobnet_run_info(std_out)
-	// 	if error != nil {
-	// 		tc_170.Err_log("Error: %s", error.Error())
-	// 		return FAILED
-	// 	}
+		// Set joabrg agent config value
+		err := lib.Jaz_set_agent_config_windows("ExtUnsignedFlag", "1")
+		if err != nil {
+			tc_168.Err_log("Failed to set joabrg agent config value. Error: %s", err.Error())
+			return FAILED
+		}
 
-	// 	if jobnet_info.Jobnet_status == "END" && jobnet_info.Job_status == "NORMAL" && jobnet_info.Exit_cd == 96 {
-	// 		return PASSED
-	// 	}
+		// Restart jobarg-agentd
+		err = lib.Restart_jaz_agent_windows()
+		if err != nil {
+			tc_168.Err_log("Failed to restart windows service. Error: %s", err.Error())
+			return FAILED
+		}
 
-	// 	tc_170.Err_log("Jobnet_status: %s, Job_status: %s, Exit_cd: %d", jobnet_info.Jobnet_status, jobnet_info.Job_status, jobnet_info.Exit_cd)
-	// 	return FAILED
-	// }
-	// tc_170.Set_function(tc_func)
-	// t.Set_testcase(*tc_170)
+		// Run jobnet
+		run_jobnet_id, error := tc_168.Jobarg_exec("TICKET1318_TESTCASE_169")
+		if error != nil {
+			tc_168.Err_log("Error: %s, std_out: %s", error.Error(), run_jobnet_id)
+			return FAILED
+		}
+
+		// Wait jobnet finishes and get jobnet run info.
+		jobnet_run_info, error := tc_168.Jobarg_get_jobnet_run_info(run_jobnet_id)
+		if error != nil {
+			tc_168.Err_log("Error: %s", error.Error())
+			return FAILED
+		}
+
+		// Check jobnet run status and exit code.
+		if jobnet_run_info.Jobnet_status == "END" && jobnet_run_info.Job_status == "NORMAL" && jobnet_run_info.Exit_cd == 100000 {
+			return PASSED
+		}
+
+		tc_168.Err_log("Jobnet_status: %s, Job_status: %s, Exit_cd: %d", jobnet_run_info.Jobnet_status, jobnet_run_info.Job_status, jobnet_run_info.Exit_cd)
+		return FAILED
+	}
+	tc_168.Set_function(tc_func)
+	t.Set_testcase(*tc_168) // Add testcase to ticket
 
 	// ticket 169
 	tc_169 := t.New_testcase(169, "Abnormal Case") // create test case
-	tc_func := func() dao.Testcase_status {
-		err := lib.Restart_jaz_agent_windows()
+	tc_func = func() dao.Testcase_status {
+
+		// Set joabrg agent config value
+		err := lib.Jaz_set_agent_config_windows("ExtUnsignedFlag", "1")
+		if err != nil {
+			tc_169.Err_log("Failed to set joabrg agent config value. Error: %s", err.Error())
+			return FAILED
+		}
+
+		// Restart jobarg-agentd
+		err = lib.Restart_jaz_agent_windows()
 		if err != nil {
 			tc_169.Err_log("Failed to restart windows service. Error: %s", err.Error())
 			return FAILED
 		}
 
-		err = lib.Stop_jaz_agent_windows()
-		if err != nil {
-			tc_169.Err_log("Failed to stop windows service. Error: %s", err.Error())
+		// Run jobnet
+		run_jobnet_id, error := tc_169.Jobarg_exec("TICKET1318_TESTCASE_169")
+		if error != nil {
+			tc_169.Err_log("Error: %s, std_out: %s", error.Error(), run_jobnet_id)
 			return FAILED
 		}
 
-		lib.Jaz_set_agent_config_windows("ExtUnsignedFlag", "1")
+		// Wait jobnet finishes and get jobnet run info.
+		jobnet_run_info, error := tc_169.Jobarg_get_jobnet_run_info(run_jobnet_id)
+		if error != nil {
+			tc_169.Err_log("Error: %s", error.Error())
+			return FAILED
+		}
 
-		return PASSED
+		// Check jobnet run status and exit code.
+		if jobnet_run_info.Jobnet_status == "END" && jobnet_run_info.Job_status == "NORMAL" && jobnet_run_info.Exit_cd == 100000 {
+			return PASSED
+		}
+
+		tc_169.Err_log("Jobnet_status: %s, Job_status: %s, Exit_cd: %d", jobnet_run_info.Jobnet_status, jobnet_run_info.Job_status, jobnet_run_info.Exit_cd)
+		return FAILED
 	}
 	tc_169.Set_function(tc_func)
 	t.Set_testcase(*tc_169) // Add testcase to ticket
+
+	// ticket 170
+	tc_170 := t.New_testcase(170, "Default Case Check. Linux AGENT")
+	tc_func = func() dao.Testcase_status {
+		std_out, error := tc_170.Jobarg_exec("TICKET_1318_linux")
+		if error != nil {
+			tc_170.Err_log("Error: %s, std_out: %s", error.Error(), std_out)
+			return FAILED
+		}
+
+		jobnet_info, error := tc_170.Jobarg_get_jobnet_run_info(std_out)
+		if error != nil {
+			tc_170.Err_log("Error: %s", error.Error())
+			return FAILED
+		}
+
+		if jobnet_info.Jobnet_status == "END" && jobnet_info.Job_status == "NORMAL" && jobnet_info.Exit_cd == 96 {
+			return PASSED
+		}
+
+		tc_170.Err_log("Jobnet_status: %s, Job_status: %s, Exit_cd: %d", jobnet_info.Jobnet_status, jobnet_info.Job_status, jobnet_info.Exit_cd)
+		return FAILED
+	}
+	tc_170.Set_function(tc_func)
+	t.Set_testcase(*tc_170)
 }
