da175698a7d7256891b577dcfad1057f876350b2
Paing Pyae Man <paingpyaeman@diracetechnology.com>
2024-10-10 13:42:38 +0630

    extract ssh client and host ips as variables

--- src/tickets/ticket_811.go
+++ src/tickets/ticket_811.go
@@ -6,6 +6,7 @@ import (
 	"github.com/zukigit/remote_run-go/src/common"
 	"github.com/zukigit/remote_run-go/src/dao"
 	"github.com/zukigit/remote_run-go/src/lib"
+	"golang.org/x/crypto/ssh"
 )
 
 type Ticket_811 struct {
@@ -56,11 +57,13 @@ func (t *Ticket_811) Set_values() {
 
 // Add your test case here
 func (t *Ticket_811) Add_testcases() {
+	serverIP := common.Login_info.Hostname
+	agentSSHClient := lib.GetSSHClient("10.1.9.212", 22, "root", "000@dirace")
 
 	// TESTCASE 69 (Force stop FWait job icon)
 	tc_69 := t.New_testcase(69, "Check Abort process abort the fwait icon (with waiting for file creation option ON) or not")
 	tc_func := func() common.Testcase_status {
-		return RunJobnetAndAbortFwaitJobIcon("10.1.9.199", "Jobnet1", 2, tc_69)
+		return RunJobnetAndAbortFwaitJobIcon(serverIP, "FileWaitJob1", 2, 5, tc_69, common.Client)
 	}
 	tc_69.Set_function(tc_func)
 	t.Add_testcase(*tc_69)
@@ -68,7 +71,7 @@ func (t *Ticket_811) Add_testcases() {
 	// TESTCASE 70 (Force stop FWait job icon on other agent)
 	tc_70 := t.New_testcase(70, "Check Abort process abort the fwait icon (with waiting for file creation option ON) or not")
 	tc_func = func() common.Testcase_status {
-		return RunJobnetAndAbort("10.1.9.198", "FWAIT_JOBNET", 2, tc_70)
+		return RunJobnetAndAbort(serverIP, "FileWaitJobOtherAgent1", 2, 5, tc_70, agentSSHClient)
 	}
 	tc_70.Set_function(tc_func)
 	t.Add_testcase(*tc_70)
@@ -76,7 +79,7 @@ func (t *Ticket_811) Add_testcases() {
 	// TESTCASE 71 (Force stop FWait jobnet with icon count of 1)
 	tc_71 := t.New_testcase(71, "Check Abort process abort the fwait icon (with waiting for file creation option ON) or not")
 	tc_func = func() common.Testcase_status {
-		return RunJobnetAndAbort("10.1.9.199", "Jobnet1", 2, tc_71)
+		return RunJobnetAndAbort(serverIP, "FileWaitJob1", 2, 5, tc_71, common.Client)
 	}
 	tc_71.Set_function(tc_func)
 	t.Add_testcase(*tc_71)
@@ -84,15 +87,15 @@ func (t *Ticket_811) Add_testcases() {
 	// TESTCASE 72 (Force stop FWait jobnet with icon count of 100)
 	tc_72 := t.New_testcase(72, "Check Abort process abort the fwait icon (with waiting for file creation option ON) or not")
 	tc_func = func() common.Testcase_status {
-		return RunJobnetAndAbort("10.1.9.199", "Jobnet100", 200, tc_72)
+		return RunJobnetAndAbort(serverIP, "FileWaitJob100", 200, 15, tc_72, common.Client)
 	}
 	tc_72.Set_function(tc_func)
-	t.Add_testcase(*tc_72)
+	// t.Add_testcase(*tc_72)
 
 	// TESTCASE 73 (Force stop FWait jobnet with icon count of 800)
 	tc_73 := t.New_testcase(73, "Check Abort process abort the fwait icon (with waiting for file creation option ON) or not")
 	tc_func = func() common.Testcase_status {
-		return RunJobnetAndAbort("10.1.9.199", "Jobnet800", 1600, tc_73)
+		return RunJobnetAndAbort(serverIP, "FileWaitJob800", 1600, 30, tc_73, common.Client)
 	}
 	tc_73.Set_function(tc_func)
 	// t.Add_testcase(*tc_73)
@@ -100,7 +103,7 @@ func (t *Ticket_811) Add_testcases() {
 }
 
 // Run the jobnet, abort it after all jobs are in running state, and confirm ENDERR status of the jobnet
-func RunJobnetAndAbort(agentIP string, jobnetId string, processCount int, testcase *dao.TestCase) common.Testcase_status {
+func RunJobnetAndAbort(agentIP string, jobnetId string, processCount int, processCheckTimeout int, testcase *dao.TestCase, sshClient *ssh.Client) common.Testcase_status {
 	// Get DB Connection as testcases inside this ticket require DB operation
 	db, err := lib.ConnectDB(lib.Postgres, agentIP, "5432", "zabbix", "zabbix", "zabbix")
 
@@ -108,10 +111,12 @@ func RunJobnetAndAbort(agentIP string, jobnetId string, processCount int, testca
 		fmt.Println(testcase.Err_log("Error connecting to the database."))
 	}
 
+	defer db.CloseDB()
+
 	// Clean the ja_run_jobnet_table
 	_, err = db.UpdateData(lib.DeleteRunJobnetQuery)
 	if err != nil {
-		testcase.Err_log("Error: %s, Failed to clean the ja_run_jobnet_table.", err.Error())
+		fmt.Println(testcase.Err_log("Error: %s, Failed to clean the ja_run_jobnet_table.", err.Error()))
 		return FAILED
 	}
 	fmt.Println(testcase.Info_log("`ja_run_jobnet_table` is cleaned."))
@@ -119,76 +124,76 @@ func RunJobnetAndAbort(agentIP string, jobnetId string, processCount int, testca
 	// Run jobnet
 	run_jobnet_id, error := lib.Jobarg_exec(jobnetId)
 	if error != nil {
-		testcase.Err_log("Error: %s, std_out: %s", error.Error(), run_jobnet_id)
+		fmt.Println(testcase.Err_log("Error: %s, std_out: %s", error.Error(), run_jobnet_id))
 		return FAILED
 	}
 	fmt.Println(testcase.Info_log("%s has been successfully run with registry number: %s", jobnetId, run_jobnet_id))
 
 	// Wait for all jobs to be in running state
-	count, err := lib.Job_process_count_check(processCount)
+	err = lib.JobProcessCountCheck(processCount, processCheckTimeout, sshClient)
 
 	if err != nil {
-		testcase.Err_log("Error: %s, Failed to get process count.", err.Error())
+		fmt.Println(testcase.Err_log("Error: %s, Failed to get process count.", err.Error()))
 		return FAILED
 	}
-	fmt.Println(testcase.Info_log("Process count has reached %d", count))
+	fmt.Println(testcase.Info_log("Process count has reached %d", processCount))
 
 	// Abort the jobnet
 	_, err = db.UpdateData(lib.AbortJobnetQuery, run_jobnet_id)
 	if err != nil {
-		testcase.Err_log("Error: %s, Failed to abort the jobnet.", err.Error())
+		fmt.Println(testcase.Err_log("Error: %s, Failed to abort the jobnet.", err.Error()))
 		return FAILED
 	}
 	fmt.Println(testcase.Info_log("Jobnet is being aborted..."))
 
 	// Wait for all jobs to be purged
 	processCount = 0
-	count, err = lib.Job_process_count_check(processCount)
+	err = lib.JobProcessCountCheck(processCount, processCheckTimeout, sshClient)
 
 	if err != nil {
-		testcase.Err_log("Error: %s, Failed to get process count.", err.Error())
+		fmt.Println(testcase.Err_log("Error: %s, Failed to get process count.", err.Error()))
 		return FAILED
 	}
-	fmt.Println(testcase.Info_log("Process count has reached %d", count))
+	fmt.Println(testcase.Info_log("Process count has reached %d", processCount))
 
 	// Wait jobnet finishes and get jobnet run info.
 	targetJobnetStatus := "ENDERR"
 	targetJobStatus := "ERROR"
 	jobnet_run_info, err := lib.Jobarg_get_jobnet_info(run_jobnet_id, targetJobnetStatus, targetJobStatus)
 	if err != nil {
-		testcase.Err_log("Error: %s", err.Error())
+		fmt.Println(testcase.Err_log("Error getting jobnet info: %s", err.Error()))
 		return FAILED
 	}
 	fmt.Println(testcase.Info_log("%s with registry number %s is completed.", jobnetId, run_jobnet_id))
 
 	// Check jobnet run status and exit code.
 	if jobnet_run_info.Jobnet_status != targetJobnetStatus {
-		testcase.Err_log("Unexpected Jobnet status. Jobnet_status: %s, Job_status: %s, Exit_cd: %d", jobnet_run_info.Jobnet_status, jobnet_run_info.Job_status, jobnet_run_info.Exit_cd)
+		fmt.Println(testcase.Err_log("Unexpected Jobnet status. Jobnet_status: %s, Job_status: %s, Exit_cd: %d", jobnet_run_info.Jobnet_status, jobnet_run_info.Job_status, jobnet_run_info.Exit_cd))
 		return FAILED
 	}
 
 	// check zombie process
-	zombieProcessCount, err := lib.CheckZombieProcess()
+	zombieProcessCount, err := lib.CheckZombieProcess(1, sshClient)
 	if err != nil {
-		testcase.Err_log("Error: %s", err.Error())
+		fmt.Println(testcase.Err_log("Error checking zombie process: %s", err.Error()))
 		return FAILED
 	}
 
 	if zombieProcessCount != 0 {
-		testcase.Err_log("There are zombie processes: %d", zombieProcessCount)
+		fmt.Println(testcase.Err_log("There are zombie processes: %d", zombieProcessCount))
 		// return FAILED
 	} else {
-		testcase.Info_log("There is no zombie process.")
+		fmt.Println(testcase.Info_log("There is no zombie process."))
 	}
 
 	// Success (obtain the expected status, message, or exit code)
-	testcase.Info_log("Jobnet_status: %s, Job_status: %s, Exit_cd: %d", jobnet_run_info.Jobnet_status, jobnet_run_info.Job_status, jobnet_run_info.Exit_cd)
+	fmt.Println(testcase.Info_log("Jobnet_status: %s, Job_status: %s, Exit_cd: %d", jobnet_run_info.Jobnet_status, jobnet_run_info.Job_status, jobnet_run_info.Exit_cd))
 
 	return PASSED
 }
 
 // Run the jobnet, abort the fwait job icon after all jobs are in running state, and confirm ENDERR status of the jobnet
-func RunJobnetAndAbortFwaitJobIcon(agentIP string, jobnetId string, processCount int, testcase *dao.TestCase) common.Testcase_status {
+func RunJobnetAndAbortFwaitJobIcon(agentIP string, jobnetId string, processCount int, processCheckTimeout int, testcase *dao.TestCase, sshClient *ssh.Client) common.Testcase_status {
 	// Get DB Connection as testcases inside this ticket require DB operation
 	db, err := lib.ConnectDB(lib.Postgres, agentIP, "5432", "zabbix", "zabbix", "zabbix")
 
@@ -199,7 +204,7 @@ func RunJobnetAndAbortFwaitJobIcon(agentIP string, jobnetId string, processCount
 	// Clean the ja_run_jobnet_table
 	_, err = db.UpdateData(lib.DeleteRunJobnetQuery)
 	if err != nil {
-		testcase.Err_log("Error: %s, Failed to clean the ja_run_jobnet_table.", err.Error())
+		fmt.Println(testcase.Err_log("Error: %s, Failed to clean the ja_run_jobnet_table.", err.Error()))
 		return FAILED
 	}
 	fmt.Println(testcase.Info_log("`ja_run_jobnet_table` is cleaned."))
@@ -207,70 +212,70 @@ func RunJobnetAndAbortFwaitJobIcon(agentIP string, jobnetId string, processCount
 	// Run jobnet
 	run_jobnet_id, error := lib.Jobarg_exec(jobnetId)
 	if error != nil {
-		testcase.Err_log("Error: %s, std_out: %s", error.Error(), run_jobnet_id)
+		fmt.Println(testcase.Err_log("Error: %s, std_out: %s", error.Error(), run_jobnet_id))
 		return FAILED
 	}
 	fmt.Println(testcase.Info_log("%s has been successfully run with registry number: %s", jobnetId, run_jobnet_id))
 
 	// Wait for all jobs to be in running state
-	count, err := lib.Job_process_count_check(processCount)
+	err = lib.JobProcessCountCheck(processCount, processCheckTimeout, sshClient)
 
 	if err != nil {
-		testcase.Err_log("Error: %s, Failed to get process count.", err.Error())
+		fmt.Println(testcase.Err_log("Error: %s, Failed to get process count.", err.Error()))
 		return FAILED
 	}
-	fmt.Println(testcase.Info_log("Process count has reached %d", count))
+	fmt.Println(testcase.Info_log("Process count has reached %d", processCount))
 
 	// Abort the jobnet
 	_, err = db.UpdateData(lib.AbortSingleFWaitJobQuery, run_jobnet_id)
 	if err != nil {
-		testcase.Err_log("Error: %s, Failed to abort the fwait job icon.", err.Error())
+		fmt.Println(testcase.Err_log("Error: %s, Failed to abort the fwait job icon.", err.Error()))
 		return FAILED
 	}
 	fmt.Println(testcase.Info_log("Fwait job icon is being aborted..."))
 
 	// Wait for all jobs to be purged
 	processCount = 0
-	count, err = lib.Job_process_count_check(processCount)
+	err = lib.JobProcessCountCheck(processCount, processCheckTimeout, sshClient)
 
 	if err != nil {
-		testcase.Err_log("Error: %s, Failed to get process count.", err.Error())
+		fmt.Println(testcase.Err_log("Error: %s, Failed to get process count.", err.Error()))
 		return FAILED
 	}
-	fmt.Println(testcase.Info_log("Process count has reached %d", count))
+	fmt.Println(testcase.Info_log("Process count has reached %d", processCount))
 
 	// Wait jobnet finishes and get jobnet run info.
 	targetJobnetStatus := "RUN"
 	targetJobStatus := "ERROR"
 	jobnet_run_info, err := lib.Jobarg_get_jobnet_info(run_jobnet_id, targetJobnetStatus, targetJobStatus)
 	if err != nil {
-		testcase.Err_log("Error: %s", err.Error())
+		fmt.Println(testcase.Err_log("Error getting jobnet info: %s", err.Error()))
 		return FAILED
 	}
 	fmt.Println(testcase.Info_log("%s with registry number %s is completed.", jobnetId, run_jobnet_id))
 
 	// Check jobnet run status and exit code.
 	if jobnet_run_info.Jobnet_status != targetJobnetStatus {
-		testcase.Err_log("Unexpected Jobnet status. Jobnet_status: %s, Job_status: %s, Exit_cd: %d", jobnet_run_info.Jobnet_status, jobnet_run_info.Job_status, jobnet_run_info.Exit_cd)
+		fmt.Println(testcase.Err_log("Unexpected Jobnet status. Jobnet_status: %s, Job_status: %s, Exit_cd: %d", jobnet_run_info.Jobnet_status, jobnet_run_info.Job_status, jobnet_run_info.Exit_cd))
 		return FAILED
 	}
 
 	// check zombie process
-	zombieProcessCount, err := lib.CheckZombieProcess()
+	zombieProcessCount, err := lib.CheckZombieProcess(1, sshClient)
 	if err != nil {
-		testcase.Err_log("Error: %s", err.Error())
+		fmt.Println(testcase.Err_log("Error checking zombie process: %s", err.Error()))
 		return FAILED
 	}
 
 	if zombieProcessCount != 0 {
-		testcase.Err_log("There are zombie processes: %d", zombieProcessCount)
+		fmt.Println(testcase.Err_log("There are zombie processes: %d", zombieProcessCount))
 		// return FAILED
 	} else {
-		testcase.Info_log("There is no zombie process.")
+		fmt.Println(testcase.Info_log("There is no zombie process."))
 	}
 
 	// Success (obtain the expected status, message, or exit code)
-	testcase.Info_log("Jobnet_status: %s, Job_status: %s, Exit_cd: %d", jobnet_run_info.Jobnet_status, jobnet_run_info.Job_status, jobnet_run_info.Exit_cd)
+	fmt.Println(testcase.Info_log("Jobnet_status: %s, Job_status: %s, Exit_cd: %d", jobnet_run_info.Jobnet_status, jobnet_run_info.Job_status, jobnet_run_info.Exit_cd))
 
 	return PASSED
 }
