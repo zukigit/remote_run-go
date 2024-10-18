5169ce4041d06218402cb67bf897489d4ecb8f89
Shin Khant <99477147+ShinKhantXoX@users.noreply.github.com>
2024-10-19 00:02:10 +0630

    fix 811 testcase 49 jobname

--- src/tickets/ticket_811.go
+++ src/tickets/ticket_811.go
@@ -161,28 +161,28 @@ func (t *Ticket_811) Add_testcases() {
 	// Run the extjon and check the abort process
 	tc_9 := t.New_testcase(9, "Check Abort process abort the Ext jobnet or not")
 	tc_func = func() common.Testcase_status {
-		return RunJobnetAndAbortExtJobIcon("TICKET811_TESTCASE49", 3, tc_9, common.Client, 100)
+		return RunJobnetAndAbortExtJobIcon("TICKET811_TESTCASE49_JOB100", 3, tc_9, common.Client, 100)
 	}
 	tc_9.Set_function(tc_func)
 	t.Add_testcase(*tc_9)
 
 	tc_10 := t.New_testcase(10, "Check Abort process abort the Ext jobnet or not")
 	tc_func = func() common.Testcase_status {
-		return RunJobnetAndAbortExtJobIcon("TICKET811_TESTCASE49", 4, tc_10, common.Client, 200)
+		return RunJobnetAndAbortExtJobIcon("TICKET811_TESTCASE49_JOB200", 4, tc_10, common.Client, 200)
 	}
 	tc_10.Set_function(tc_func)
 	t.Add_testcase(*tc_10)
 
 	tc_11 := t.New_testcase(11, "Check Abort process abort the Ext jobnet or not")
 	tc_func = func() common.Testcase_status {
-		return RunJobnetAndAbortExtJobIcon("TICKET811_TESTCASE49", 5, tc_11, common.Client, 400)
+		return RunJobnetAndAbortExtJobIcon("TICKET811_TESTCASE49_JOB400", 5, tc_11, common.Client, 400)
 	}
 	tc_11.Set_function(tc_func)
 	t.Add_testcase(*tc_11)
 
 	tc_12 := t.New_testcase(12, "Check Abort process abort the Ext jobnet or not")
 	tc_func = func() common.Testcase_status {
-		return RunJobnetAndAbortExtJobIcon("TICKET811_TESTCASE49", 5, tc_12, common.Client, 800)
+		return RunJobnetAndAbortExtJobIcon("TICKET811_TESTCASE49_JOB800", 5, tc_12, common.Client, 800)
 	}
 	tc_12.Set_function(tc_func)
 	t.Add_testcase(*tc_12)
