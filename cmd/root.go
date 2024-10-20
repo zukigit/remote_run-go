7dea7527ad8032a93f53a5761a139c55783e0687
zuki <zukidono@gmail.com>
2024-10-20 23:37:57 +0700

    removed 000

--- cmd/root.go
+++ cmd/root.go
@@ -216,7 +216,7 @@ func init() {
 // Add your tickets here
 func add_tickets(t *[]dao.Ticket) {
 	*t = append(*t, new(tickets.Ticket_1318))
-	*t = append(*t, new(tickets.Ticket_000))
+	// *t = append(*t, new(tickets.Ticket_000))
 	*t = append(*t, new(tickets.Ticket_811))
 	*t = append(*t, new(tickets.Ticket_800))
 	// *t = append(*t, new(tickets.Ticket_1225))
