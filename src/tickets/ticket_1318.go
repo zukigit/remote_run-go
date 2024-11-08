a3ab3fbd1d772425a83e280ddca60ac9bd36b276
zuki <zukidono@gmail.com>
2024-11-09 01:27:48 +0700

    implemented ticker 1318

--- src/tickets/ticket_1318.go
+++ src/tickets/ticket_1318.go
@@ -7,9 +7,10 @@ import (
 )
 
 type Ticket_1318 struct {
-	no          uint
-	description string
-	testcases   []dao.TestCase
+	Ticket_no                                   uint
+	Ticket_description                          string
+	PASSED_count, FAILED_count, MUSTCHECK_count int
+	Testcases                                   []dao.TestCase
 }
 
 func (t *Ticket_1318) New_testcase(testcase_id uint, testcase_description string) *dao.TestCase {
@@ -17,24 +18,36 @@ func (t *Ticket_1318) New_testcase(testcase_id uint, testcase_description string
 }
 
 func (t *Ticket_1318) Get_no() uint {
-	return t.no
+	return t.Ticket_no
+}
+
+func (t *Ticket_1318) Set_PASSED_count(passed_count int) {
+	t.PASSED_count = passed_count
+}
+
+func (t *Ticket_1318) Set_FAILED_count(failed_count int) {
+	t.FAILED_count = failed_count
+}
+
+func (t *Ticket_1318) Set_MUSTCHECK_count(mustcheck_count int) {
+	t.MUSTCHECK_count = mustcheck_count
 }
 
 func (t *Ticket_1318) Get_dsctn() string {
-	return t.description
+	return t.Ticket_description
 }
 
 func (t *Ticket_1318) Add_testcase(tc dao.TestCase) {
-	t.testcases = append(t.testcases, tc)
+	t.Testcases = append(t.Testcases, tc)
 }
 
 func (t *Ticket_1318) Get_testcases() []dao.TestCase {
-	return t.testcases
+	return t.Testcases
 }
 
 func (t *Ticket_1318) Set_values() {
-	t.no = 1318
-	t.description = "Fixed for negative JOB_EXT_CD return value."
+	t.Ticket_no = 1318
+	t.Ticket_description = "Fixed for negative JOB_EXT_CD return value."
 }
 
 // Add your testcase here
