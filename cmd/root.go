310fd4a49e4b47099b99b1a4d904fb061bb72ccd
zuki <zukidono@gmail.com>
2024-11-09 01:34:20 +0700

    implemented new rd 794

--- cmd/root.go
+++ cmd/root.go
@@ -173,5 +173,5 @@ func add_tickets(t *[]dao.Ticket) {
 	*t = append(*t, new(tickets.Ticket_800))
 	*t = append(*t, new(tickets.Ticket_1225))
 	*t = append(*t, new(tickets.Ticket_844))
-	// *t = append(*t, new(tickets.Ticket_794))
+	*t = append(*t, new(tickets.Ticket_794))
 }
